e0d93cf9485f505d3a83a0c4bed4926629479aa5
kaiyuan zhao <kaiyuan941014@gmail.com>
2021-06-07 13:53:53 -0400

    port || 5000

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
     HOST: process.env.DB_HOST,
-    PORT: process.env.DB_PORT,
+    PORT: process.env.DB_PORT || 5000,
     USER: process.env.DB_USER,
     PASSWORD: process.env.DB_PASSWORD,
     DATABASE: process.env.DB_SCHEMA,
